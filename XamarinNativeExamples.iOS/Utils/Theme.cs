4be9078cc869a6456f63787e70659254eac24da9
Jerome Manzano <jeromemanzano@gmail.com>
2020-09-15 10:51:52 +0800

    add theme

---
+++ XamarinNativeExamples.iOS/Utils/Theme.cs
@@ -0,0 +1,12 @@
+﻿using UIKit;
+
+namespace XamarinNativeExamples.iOS.Utils
+{
+    public static class Theme
+    {
+        public static void Home()
+        {
+            UIView.Appearance.TintColor = UIColor.FromName("PrussianBlue");
+        }
+    }
+}
