3e73a4fedebb91a32c5e0ec3ff2851c2ad9d2faa
Jerome Manzano <jeromemanzano@gmail.com>
2020-09-15 09:21:34 +0800

    implement LaunchScreen

---
+++ XamarinNativeExamples.iOS/Views/Home/HomeView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+
+namespace XamarinNativeExamples.iOS.Views.Home
+{
+    class HomeView
+    {
+    }
+}
