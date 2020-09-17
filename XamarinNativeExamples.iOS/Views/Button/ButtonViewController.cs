a597e338ebae04f1af4dca951ad1cf44b4dac1e8
Jerome Manzano <astromobilesolutions@gmail.com>
2020-09-16 19:00:30 -0700

    add Button Screen

---
+++ XamarinNativeExamples.iOS/Views/Button/ButtonViewController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using UIKit;
+using XamarinNativeExamples.Core.ViewModels.Button;
+using XamarinNativeExamples.iOS.Utils;
+using XamarinNativeExamples.iOS.Views.Base;
+
+namespace XamarinNativeExamples.iOS.Views.Button
+{
+    public partial class ButtonViewController : BaseViewController<ButtonViewModel>
+    {
+        protected override Theme Theme => Themes.Home;
+
+        public ButtonViewController() : base("ButtonViewController")
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+        }
+
+        protected override void InitializeControls()
+        {
+            base.InitializeControls();
+
+            EdgesForExtendedLayout = UIRectEdge.None;
+        }
+    }
+}
+
