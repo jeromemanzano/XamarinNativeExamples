2dd1af51f4d41e56c6542dd975c594eae8a69ffe
Jerome Manzano <astromobilesolutions@gmail.com>
2020-09-18 00:39:56 -0700

    add ButtonClickViewController and ButtonEnableViewController

---
+++ XamarinNativeExamples.iOS/Views/Button/ButtonClickViewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UIKit;
+
+namespace XamarinNativeExamples.iOS.Views.Button
+{
+    public partial class ButtonClickViewController : UIViewController
+    {
+        public ButtonClickViewController() : base("ButtonClickViewController", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
