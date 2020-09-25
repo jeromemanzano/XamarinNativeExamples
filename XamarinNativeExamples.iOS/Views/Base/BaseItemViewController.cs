db7696b53f043d9c202353ca54910bf456b67425
Jerome Manzano <jeromemanzano@gmail.com>
2020-09-25 08:20:44 +0800

    add BaseItemViewController

---
+++ XamarinNativeExamples.iOS/Views/Base/BaseItemViewController.cs
@@ -0,0 +1,41 @@
+﻿using MvvmCross.Platforms.Ios.Views;
+using MvvmCross.ViewModels;
+
+namespace XamarinNativeExamples.iOS.Views.Base
+{
+    public abstract class BaseItemViewController<TViewModel> : MvxViewController<TViewModel>
+        where TViewModel : class, IMvxViewModel
+    {
+        protected BaseItemViewController()
+        {
+        }
+
+        protected BaseItemViewController(string nibName)
+            : base(nibName, null)
+        {
+        }
+
+        protected virtual void InitializeControls() { }
+
+        protected virtual void BindControls() { }
+
+        protected virtual void Cleanup() { }
+
+        protected virtual void ApplyTheme() { }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            InitializeControls();
+            ApplyTheme();
+            BindControls();
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+        }
+
+    }
+}
