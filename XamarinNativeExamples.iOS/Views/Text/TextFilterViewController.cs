34ea8099f4a2da0794ba8342d58da5816ebb1d4c
Jerome Manzano <astromobilesolutions@gmail.com>
2020-09-24 20:18:04 -0700

    add TextViewController, TextTextViewController and TextFilterViewController

---
+++ XamarinNativeExamples.iOS/Views/Text/TextFilterViewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UIKit;
+
+namespace XamarinNativeExamples.iOS.Views.Text
+{
+    public partial class TextFilterViewController : UIViewController
+    {
+        public TextFilterViewController() : base("TextFilterViewController", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
---
+++ XamarinNativeExamples.iOS/Views/Text/TextViewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UIKit;
+
+namespace XamarinNativeExamples.iOS.Views.Text
+{
+    public partial class TextViewController : UIViewController
+    {
+        public TextViewController() : base("TextViewController", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
