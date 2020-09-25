d164f0781ca50863873a78d62eb5ef81c742d76c
Jerome Manzano <jeromemanzano@gmail.com>
2020-09-25 08:21:35 +0800

    add binding for Long Press

---
+++ XamarinNativeExamples.iOS/Extensions/MvxBehaviorExtensions.cs
@@ -0,0 +1,12 @@
+﻿using XamarinNativeExamples.iOS.Views.Gestures;
+
+namespace UIKit
+{
+    public static class MvxBehaviorExtensions
+    {
+        public static MvxLongPressGestureRecognizerBehavior LongPress(this UIView view)
+        {
+            return new MvxLongPressGestureRecognizerBehavior(view);
+        }
+    }
+}
---
+++ XamarinNativeExamples.iOS/Views/Gestures/MvxLongPressGestureRecognizerBehavior.cs
@@ -0,0 +1,24 @@
+﻿using MvvmCross.Platforms.Ios.Binding.Views.Gestures;
+using UIKit;
+
+namespace XamarinNativeExamples.iOS.Views.Gestures
+{
+    public class MvxLongPressGestureRecognizerBehavior
+    : MvxGestureRecognizerBehavior<UILongPressGestureRecognizer>
+    {
+        public MvxLongPressGestureRecognizerBehavior(UIView target)
+        {
+            var lp = new UILongPressGestureRecognizer(HandleGesture);
+
+            AddGestureRecognizer(target, lp);
+        }
+
+        protected override void HandleGesture(UILongPressGestureRecognizer gesture)
+        {
+            if (gesture.State == UIGestureRecognizerState.Began)
+            {
+                FireCommand();
+            }
+        }
+    }
+}
