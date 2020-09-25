4b5ccb4c6086ad09e6900505657043dc5e2c4deb
Jerome Manzano <jeromemanzano@gmail.com>
2020-09-25 08:23:02 +0800

    update Themes implementation

--- XamarinNativeExamples.iOS/Utils/Theme.cs
+++ XamarinNativeExamples.iOS/Utils/Theme.cs
@@ -1,31 +1,82 @@
-﻿using UIKit;
+﻿using System;
+using UIKit;
 
 namespace XamarinNativeExamples.iOS.Utils
 {
-    public struct Theme
-    {
-        public UIColor NavigationBackground { get; set; }
-        public UIColor NavigationTextColor { get; set; }
-        public UIColor ButtonBackgroundColor { get; set; }
-        public UIColor ButtonTextColor { get; set; }
-        public UIImage LargeTitleNavigationBackground { get; set; }
+    public struct ButtonTheme 
+    { 
+        public UIColor BackgroundColor { get; set; }
+        public UIColor TextColor { get; set; }
+        public UIColor SelectedBackgroundColor { get; set; }
+        public UIColor SelectedTextColor { get; set; }
+        public UIColor DisabledBackgroundColor { get; set; }
+        public UIColor DisabledTextColor { get; set; }
+        public nfloat CornerRadius { get; set; }
+        public UIFont Font { get; set; }
     }
 
-    public class Themes
+    public struct NavigationTheme 
+    { 
+        public bool PrefersLargeTitles { get; set; }
+        public UIImage BackgroundImage { get; set; }
+        public UIColor BackgroundColor { get; set; }
+        public UIColor TextColor { get; set; }
+    }
+
+    public struct AccordionTheme 
+    { 
+        public UIFont TitleFont { get; set; }
+        public UIColor TitleTextColor { get; set; }
+        public UIColor ToggleBackgroundColor { get; set; }
+        public UIColor ToggleTextColor { get; set; }
+        public UIColor BackgroundColor { get; set; }
+        public nfloat CornerRadius { get; set; }
+    }
+
+    public struct ViewControllerTheme 
     {
-        public static Theme Home => new Theme 
+        public ButtonTheme ButtonTheme { get; set; }
+        public NavigationTheme NavigationTheme { get; set; }
+        public AccordionTheme AccordionTheme { get; set; }
+    }
+
+    public static class Themes
+    {
+        public static ViewControllerTheme Home => new ViewControllerTheme 
         {
-            NavigationBackground = UIColor.FromName("PrussianBlue"),
-            NavigationTextColor = UIColor.White
+            NavigationTheme = new NavigationTheme 
+            { 
+                BackgroundColor = UIColor.FromName("PrussianBlue"),
+                TextColor = UIColor.White
+            }
         };
 
-        public static Theme Button => new Theme
+        public static ViewControllerTheme Button => new ViewControllerTheme
         {
-            NavigationBackground = UIColor.FromName("MustardYellow"),
-            NavigationTextColor = UIColor.White,
-            ButtonBackgroundColor = UIColor.FromName("TimberGreen"),
-            ButtonTextColor = UIColor.White,
-            LargeTitleNavigationBackground = UIImage.FromBundle("ButtonPanel")
+            NavigationTheme = new NavigationTheme
+            {
+                BackgroundColor = UIColor.FromName("MustardYellow"),
+                TextColor = UIColor.White,
+                PrefersLargeTitles = true,
+                BackgroundImage = UIImage.FromBundle("ButtonPanel").SetAlpha(0.6f, CoreGraphics.CGBlendMode.Screen),
+            },
+            ButtonTheme = new ButtonTheme 
+            {
+                BackgroundColor = UIColor.FromName("TimberGreen"),
+                TextColor = UIColor.White,
+                DisabledBackgroundColor = UIColor.LightGray,
+                DisabledTextColor = UIColor.DarkTextColor,
+                CornerRadius = 5f,
+                Font = UIFont.SystemFontOfSize(20f, UIFontWeight.Bold)
+            },
+            AccordionTheme = new AccordionTheme 
+            { 
+                TitleTextColor = UIColor.Black,
+                TitleFont  = UIFont.SystemFontOfSize(20f, UIFontWeight.Bold),
+                ToggleBackgroundColor = UIColor.FromName("MustardYellow"),
+                ToggleTextColor = UIColor.White,
+                CornerRadius = 5f
+            }
         };
     }
 }
