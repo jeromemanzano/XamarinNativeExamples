4d7d645be848df82c019fa96fda32790ea72caf5
Jerome Manzano <jeromemanzano@gmail.com>
2020-09-25 08:24:03 +0800

    Update HomeViewController to use latest theme

--- XamarinNativeExamples.iOS/Views/Home/HomeViewController.cs
+++ XamarinNativeExamples.iOS/Views/Home/HomeViewController.cs
@@ -8,22 +8,11 @@ namespace XamarinNativeExamples.iOS.Views.Home
 {
     public partial class HomeViewController : BaseViewController<HomeViewModel>
     {
-        protected override Theme Theme => Themes.Home;
+        protected override ViewControllerTheme Theme => Themes.Home;
 
         public HomeViewController() : base("HomeViewController")
         {
         }
-
-        public override void ViewDidLoad()
-        {
-            base.ViewDidLoad();
-        }
-
-        public override void DidReceiveMemoryWarning()
-        {
-            base.DidReceiveMemoryWarning();
-        }
-
         protected override void InitializeControls()
         {
             base.InitializeControls();
