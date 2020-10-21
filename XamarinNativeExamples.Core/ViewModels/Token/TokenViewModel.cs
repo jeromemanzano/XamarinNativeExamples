cd1e94def1aab91ac0d56e896ccdc64b042640bf
Jerome Manzano <jeromemanzano@gmail.com>
2020-10-21 16:42:15 +0800

    implement token screen

---
+++ XamarinNativeExamples.Core/ViewModels/Token/TokenViewModel.cs
@@ -0,0 +1,119 @@
+﻿using System.Threading.Tasks;
+using MvvmCross;
+using MvvmCross.Commands;
+using XamarinNativeExamples.Core.Managers.Stocks;
+using XamarinNativeExamples.Core.Properties;
+using XamarinNativeExamples.Core.ViewModels.Base;
+
+namespace XamarinNativeExamples.Core.ViewModels.Token
+{
+    public class TokenViewModel : BaseResultPageViewModel<bool>
+    {
+        private readonly IStockManager _stockManager;
+
+        public TokenViewModel() 
+        { 
+            _stockManager = Mvx.IoCProvider.Resolve<IStockManager>();
+        }
+
+        public override Task Initialize()
+        {
+            TestButtonText = Resources.Test;
+            ApiTextEnabled = true;
+
+            return base.Initialize();
+        }
+
+        public override string Title => string.Empty;
+
+        public string TokenStartText => Resources.TokenStartMessage;
+
+        public string TokenRegisterText => Resources.TokenRegisterMessage;
+
+        public string TokenCopyText => Resources.TokenCopyMessage;
+
+        public string TokenTestText => Resources.TokenTestMessage;
+
+        public string TokenNextText => Resources.Next;
+
+        public string ApiKeyPlaceholder => Resources.ApiKeyPlaceholder;
+
+        public string TokenFailedText => Resources.TokenFailedMessage;
+
+        public string TokenSuccessText => Resources.TokenSuccessMessage;
+
+        private string _testButtonText;
+        public string TestButtonText
+        {
+            get => _testButtonText;
+            private set => SetProperty(ref _testButtonText, value);
+        }
+
+        private string _apiToken;
+        public string ApiToken
+        {
+            get => _apiToken;
+            private set => SetProperty(ref _apiToken, value);
+        }
+
+        private bool _tokenValid;
+        public bool TokenValid
+        {
+            get => _tokenValid;
+            private set => SetProperty(ref _tokenValid, value);
+        }
+
+        private bool _tokenTested;
+        public bool TokenTested
+        {
+            get => _tokenTested;
+            private set => SetProperty(ref _tokenTested, value);
+        }
+
+        private bool _apiTextEnabled;
+        public bool ApiTextEnabled
+        {
+            get => _apiTextEnabled;
+            private set => SetProperty(ref _apiTextEnabled, value);
+        }
+
+        private bool _showFailed;
+        public bool ShowFailed
+        {
+            get => _showFailed;
+            private set => SetProperty(ref _showFailed, value);
+        }
+
+        private bool _showSuccess;
+        public bool ShowSuccess
+        {
+            get => _showSuccess;
+            private set => SetProperty(ref _showSuccess, value);
+        }
+
+        private IMvxCommand _saveCommand;
+        public IMvxCommand SaveCommand
+        {
+            get => _saveCommand ?? (_saveCommand = new MvxAsyncCommand(Save));
+        }
+
+        private async Task Save()
+        {
+            if (TokenValid)
+            {
+                await _stockManager.UpdateToken(ApiToken);
+                await Navigation.Close<bool>(this, true);
+            }
+            else 
+            {
+                TokenValid = await _stockManager.ValidateToken(ApiToken);
+                TestButtonText = TokenValid ? Resources.Save : Resources.Test;
+                ApiTextEnabled = false;
+                ShowSuccess = TokenValid;
+                ShowFailed = !TokenValid;
+            }
+
+            TokenTested = true;
+        }
+    }
+}
---
+++ XamarinNativeExamples.UITest/Pages/TokenPage.cs
@@ -0,0 +1,67 @@
+﻿using Query = System.Func<Xamarin.UITest.Queries.AppQuery, Xamarin.UITest.Queries.AppQuery>;
+
+namespace XamarinNativeExamples.UITest.Pages
+{
+    public class TokenPage : BasePage
+    {
+        private readonly Query startScreen;
+        private readonly Query registerScreen;
+        private readonly Query copyScreen;
+        private readonly Query tokenField;
+        private readonly Query submitButton;
+        private readonly Query successText;
+
+        protected override PlatformQuery Trait => new PlatformQuery
+        {
+            Android = x => x.Id("img_api_token"),
+            iOS = x => x.Marked("")
+        };
+
+        public TokenPage()
+        {
+            if (OnAndroid)
+            {
+                startScreen = x => x.Id("img_api_token");
+                registerScreen = x => x.Id("img_api_register");
+                copyScreen = x => x.Id("img_copy_token");
+                tokenField = x => x.Id("token_edit_text");
+                submitButton = x => x.Id("save_button");
+                successText = x => x.Id("success_text");
+            }
+
+            if (OniOS)
+            {
+            }
+        }
+
+        public TokenPage SwipeToInput()
+        {
+            App.SwipeRightToLeft();
+            App.WaitForElement(registerScreen);
+            App.SwipeRightToLeft();
+            App.WaitForElement(copyScreen);
+            App.SwipeRightToLeft();
+
+            return this;
+        }
+
+        public TokenPage TestApiKey(string apiKey)
+        {
+            if (OnAndroid)
+            {
+                App.Query(x => x.Id("token_edit_text")
+                .Invoke("setText", apiKey));
+            }
+
+            App.Tap(submitButton);
+
+            return this;
+        }
+
+        public void SaveToken() 
+        {
+            App.WaitForElement(successText);
+            App.Tap(submitButton);
+        }
+    }
+}
