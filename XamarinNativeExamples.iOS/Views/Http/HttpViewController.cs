cdf6cad2356aa8353901f928d600e0ccf7906889
Jerome Manzano <astromobilesolutions@gmail.com>
2020-10-23 04:40:01 -0700

    add View and ViewController for Http and Token

---
+++ XamarinNativeExamples.iOS/Views/Http/HttpViewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UIKit;
+
+namespace XamarinNativeExamples.iOS.Views.Http
+{
+    public partial class HttpViewController : UIViewController
+    {
+        public HttpViewController() : base("HttpViewController", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
