a651f586a80a22a18febeeb23559498b93bfda23
Jerome Manzano <jeromemanzano@gmail.com>
2020-10-27 21:08:55 +0800

    implement WebSocket UI in iOS

--- XamarinNativeExamples.iOS/Views/WebSocket/WebSocketViewController.cs
+++ XamarinNativeExamples.iOS/Views/WebSocket/WebSocketViewController.cs
@@ -1,25 +1,76 @@
-﻿using System;
-
-using UIKit;
+﻿using UIKit;
+using XamarinNativeExamples.iOS.Utils;
+using XamarinNativeExamples.iOS.Views.Base;
+using MvvmCross.Binding.BindingContext;
+using XamarinNativeExamples.Core.ViewModels.WebSocket;
 
 namespace XamarinNativeExamples.iOS.Views.WebSocket
 {
-    public partial class WebSocketViewController : UIViewController
+    public partial class WebSocketViewController : BaseViewController<WebSocketViewModel>
     {
-        public WebSocketViewController() : base("WebSocketViewController", null)
+        protected override ViewControllerTheme Theme => Themes.WebSocket;
+
+        public WebSocketViewController() : base("WebSocketViewController")
         {
         }
 
-        public override void ViewDidLoad()
+        protected override void InitializeControls()
         {
-            base.ViewDidLoad();
-            // Perform any additional setup after loading the view, typically from a nib.
+            base.InitializeControls();
+
+            EdgesForExtendedLayout = UIRectEdge.None;
+
+            ConnectButton.ApplyTheme(Theme.ButtonTheme);
+            SubscribeButton.ApplyTheme(Theme.ButtonTheme);
         }
 
-        public override void DidReceiveMemoryWarning()
+        protected override void BindControls()
         {
-            base.DidReceiveMemoryWarning();
-            // Release any cached data, images, etc that aren't in use.
+            base.BindControls();
+
+            var set = this.CreateBindingSet<WebSocketViewController, WebSocketViewModel>();
+
+            set.Bind(ConnectButton).To(vm => vm.ConnectionCommand);
+            set.Bind(ConnectButton)
+                .For("Title")
+                .To(vm => vm.ConnectButtonText);
+
+            set.Bind(SubscribeButton).To(vm => vm.SubscribeCommand);
+            set.Bind(SubscribeButton)
+                .For("Visibility")
+                .To(vm => vm.ShowStockDetails)
+                .WithConversion("Visibility");
+            set.Bind(SubscribeButton)
+                .For("Title")
+                .To(vm => vm.SubscribeText);
+
+            set.Bind(PingLabel).To(vm => vm.PingCount);
+
+            set.Bind(SymbolInput).To(vm => vm.InputStockSymbol);
+            set.Bind(SymbolInput)
+                .For("Visibility")
+                .To(vm => vm.ShowStockDetails)
+                .WithConversion("Visibility");
+
+            set.Bind(PriceLabel).To(vm => vm.Price);
+            set.Bind(PriceLabel)
+                .For("Visibility")
+                .To(vm => vm.ShowStockDetails)
+                .WithConversion("Visibility");
+
+            set.Bind(VolumeLabel).To(vm => vm.Volume);
+            set.Bind(VolumeLabel)
+                .For("Visibility")
+                .To(vm => vm.ShowStockDetails)
+                .WithConversion("Visibility");
+
+            set.Bind(TimeLabel).To(vm => vm.Time);
+            set.Bind(TimeLabel)
+                .For("Visibility")
+                .To(vm => vm.ShowStockDetails)
+                .WithConversion("Visibility");
+
+            set.Apply();
         }
     }
 }
