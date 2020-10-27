f48bddef39d97b0778c16d4d900852baf346697c
Jerome Manzano <astromobilesolutions@gmail.com>
2020-10-27 05:19:19 -0700

    add WebSocketViewController

---
+++ XamarinNativeExamples.iOS/Views/WebSocket/WebSocketViewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UIKit;
+
+namespace XamarinNativeExamples.iOS.Views.WebSocket
+{
+    public partial class WebSocketViewController : UIViewController
+    {
+        public WebSocketViewController() : base("WebSocketViewController", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
