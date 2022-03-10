0b68370270c455ab2427c03d515184a8180dbfab
Jerome Manzano <jeromemanzano@gmail.com>
2022-03-10 21:17:00 +0800

    Update iOS MvvmCross to 8.0.2 and install Serilog

--- XamarinNativeExamples.iOS/Setup.cs
+++ XamarinNativeExamples.iOS/Setup.cs
@@ -1,9 +1,12 @@
-﻿using MvvmCross;
-using MvvmCross.Binding.BindingContext;
+﻿using Microsoft.Extensions.Logging;
+using MvvmCross;
 using MvvmCross.Binding.Bindings.Target.Construction;
 using MvvmCross.Converters;
+using MvvmCross.IoC;
 using MvvmCross.Platforms.Ios.Core;
 using MvvmCross.Platforms.Ios.Presenters;
+using Serilog;
+using Serilog.Extensions.Logging;
 using UIKit;
 using XamarinNativeExamples.Core;
 using XamarinNativeExamples.Core.Services.Interactions;
@@ -20,13 +23,11 @@ namespace XamarinNativeExamples.iOS
             return new CustomPresenter(ApplicationDelegate as UIApplicationDelegate, Window);
         }
 
-        protected override void InitializeFirstChance()
+        protected override void InitializeFirstChance(IMvxIoCProvider iocProvider)
         {
-            base.InitializeFirstChance();
-
-            Mvx.IoCProvider.RegisterSingleton<IDialogService>(() => Mvx.IoCProvider.IoCConstruct<DialogService>());
+            base.InitializeFirstChance(iocProvider);
+            iocProvider.RegisterSingleton<IDialogService>(() => Mvx.IoCProvider.IoCConstruct<DialogService>());
         }
-
         protected override void FillTargetFactories(IMvxTargetBindingFactoryRegistry registry)
         {
             base.FillTargetFactories(registry);
@@ -40,6 +41,20 @@ namespace XamarinNativeExamples.iOS
 
             registry.AddOrOverwrite("Visibility", new MvvmCross.Plugin.Visibility.MvxVisibilityValueConverter());
             registry.AddOrOverwrite("CheckIconConverter", new BoolToCheckIconConverter());
+        }
+        
+        protected override ILoggerProvider CreateLogProvider()
+        {
+            return new SerilogLoggerProvider();
+        }
+        
+        protected override ILoggerFactory CreateLogFactory()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .CreateLogger();
+
+            return new SerilogLoggerFactory();
         }
     }
 }
